b0ecd694a6efae525507989fe642afa3a67645cd
BillyGalbreath <Blake.Galbreath@GMail.com>
2018-08-31 08:49:44 -0500

    Travis-CI attempt #3

--- installPaper.sh
+++ installPaper.sh
@@ -4,6 +4,8 @@ git clone https://github.com/PaperMC/Paper.git
 pushd $HOME/Paper
 git config --global user.email "jenkins@user.com"
 git config --global user.name "jenkins"
+git checkout pre/1.13
 echo "Building Paper for minecraft version $1 (this might take a while)"
+paper patch
 paper jar
 popd
